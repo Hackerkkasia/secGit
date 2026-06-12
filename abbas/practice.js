517adf66280a36e65ad5886ca6d596c8c527bbc9
Hackerkkasia <Kaifkabgmi177@gmail.com>
2026-06-12 09:09:51 -0400

    index

---
+++ abbas/practice.js
@@ -0,0 +1,5 @@
+num1 = parseInt(prompt("Enter the first number:"))
+    num2 = parseInt(prompt("Enter the second number:"))
+    console.log("sum:" + (num1 + num2)
+    console.log("multiplicaation:"+(num1*num2))
+    
